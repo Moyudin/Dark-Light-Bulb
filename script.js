40e47dcca9e17a952645fd7a321f43be5f5a67ac
Moyudin Papu <moinpapu92446@gmail.com>
2023-11-29 18:32:52 +0530

    I have completed with Light Bulb

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $('#col-img2').click(function() {
+    var image2 = $('#col-img2');
+    var Of2 = $('#ON-OFF2');
+    if (image2.hasClass('gray-Background')) {
+      image2.removeClass('gray-Background');
+      image2.addClass('yellow-Background');
+      Of2.text("Bulb is ON")
+    } else {
+      image2.removeClass('yellow-Background');
+      image2.addClass('gray-Background');
+      Of2.text("Bulb is OFF")
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('#col-img3').click(function() {
+    var image3 = $('#col-img3');
+    var Of3 = $('#ON-OFF3');
+    if (image3.hasClass('gray-Background')) {
+      image3.removeClass('gray-Background');
+      image3.addClass('red-Background');
+      Of3.text("Bulb is ON")
+    } else {
+      image3.removeClass('red-Background');
+      image3.addClass('gray-Background');
+      Of3.text("Bulb is OFF")
+    }
+  });
+});
